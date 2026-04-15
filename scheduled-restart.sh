f017801a3ed7ad9289137316772d77fb47554f47
Keegan Conlee <dacid44@gmail.com>
2026-04-15 12:18:49 -0500

    make scheduled restart script work with docker

--- scheduled-restart.sh
+++ scheduled-restart.sh
@@ -2,8 +2,19 @@
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source "$SCRIPT_DIR/.env"
 
+if [[ -x "$(command -v docker)" ]] && docker inspect -f '{{.State.Status}}' $CONTAINER_NAME; then
+    if ! [[ -x "$(command -v script)" ]]; then
+        echo "script command not found, please install util-linux[-script]"
+    fi
+    IS_DOCKER=1
+fi
+
 send_command() {
-    tmux send-keys -t "$TMUX_SESSION" "$1" Enter
+    if [[ "$IS_DOCKER" == "1" ]]; then
+        echo "$1" | script -E never -qefc 'docker attach --detach-keys=ctrl-d starmade' /dev/null
+    else
+        tmux send-keys -t "$TMUX_SESSION" "$1" Enter
+    fi
 }
 
 echo "$(date) - Starting scheduled daily restart..." >> "$LOG_DIR/restart.log"
@@ -13,7 +24,9 @@ send_command "/server_message_broadcast warning \"Server will restart for daily 
 sleep 60
 
 send_command "/shutdown 0"
-sleep 5
-sudo systemctl restart "$SYSTEMCTL_SERVICE"
+if [[ "$IS_DOCKER" != "1" ]]; then
+    sleep 5
+    sudo systemctl restart "$SYSTEMCTL_SERVICE"
+fi
 
 echo "$(date) - Daily restart complete." >> "$LOG_DIR/restart.log"
